8dc45af0128888a3692f7f0ec64e3d405568a159
fwang1395 <love5426986w@163.com>
2018-06-29 10:55:56 +0800

    Implement strStr
    Implement strStr，我的方法有点复杂，耗时长。

--- python/String/ImplementstrSt.py
+++ python/String/ImplementstrSt.py
@@ -20,6 +20,11 @@ What should we return when needle is an empty string? This is a great question t
 
 For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf()."""
 class Solution(object):
+    """
+我的有点复杂，耗时长
+You are here! 
+Your runtime beats 7.67 % of python submissions.
+    """
     def strStr(self, haystack, needle):
         """
         :type haystack: str
@@ -30,30 +35,56 @@ class Solution(object):
         len_haystack = len(haystack)
         if len_needle <1 :
             return 0
+        elif len_haystack < len_needle:
+            return -1
         i,j = 0,0
-        while i< len_haystack:
-            print "while i:%d--------------"%i
+        while i< len_haystack and (i + len_needle) <= len_haystack:
+            print "i=%d"%(i)
             plus = 0
-            while j < len_needle:
-                if i< len_haystack and j < (len_needle-1) and haystack[i] == needle[j]:
-                    print "11111111"
+            while j < len_needle and i< len_haystack:
+                if  j < (len_needle-1) and haystack[i] == needle[j]:
+                    print "if"
                     plus += 1
                     i+=1
                     j+=1
-
-                    print "while i=%d,j=%d,plus=%d"%(i,j,plus)
-                elif i< len_haystack and j == (len_needle-1) and haystack[i] == needle[j]:
-                    print "222222"
-                    print "while i=%d,j=%d,plus=%d"%(i,j,plus)
+                elif j == (len_needle-1) and haystack[i] == needle[j]:
+                    print "elif"
                     return i-plus
                 else:
-                    print "333333"
+                    print "else"
                     j = 0
                     i = i+1-plus
-                    print "while i=%d,j=%d,plus=%d"%(i,j,plus)
                     break
-        if i>=len_haystack:
+        print i
+        return -1 if (i+len_needle)>len_haystack else i
+    """
+    sample 30 ms submission
+    """
+    def strStr1(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        s = haystack
+        w = needle
+        l1 = len(s)
+        l2 = len(w)
+
+        #res = -1
+        #ix = 0
+        #ix2 = 0
+
+        if l1 - l2 < 0:
             return -1
+
+        for i in range(0, l1-l2+1):
+            w2 = s[i:i+l2]
+            #print w
+            #print w2
+            if w == w2:
+                return i
+        return -1
 if __name__ == '__main__':
     solution = Solution()
     haystack = "mississippi"
