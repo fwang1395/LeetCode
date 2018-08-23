2c4c58a5105cc7f7f5e821a8b0b12bd7e3e08a2b
fei.wang <fei.wang@chukong-inc.com>
2018-08-23 11:00:39 +0800

    增加判断是否是二叉搜索树BST

--- php/Trees/sortedArrayToBST.php
+++ php/Trees/sortedArrayToBST.php
@@ -3,13 +3,13 @@ class TreeNode
 {
 	public function __construct($nodevalue){
 		$this->value = $nodevalue;
-		$this->left = None;
-		$this->right = None;
+		$this->left = Null;
+		$this->right = Null;
 	}
 }
 
 function sortedArrayToBST($list,$left,$right){
-	if($right < $left) return None;
+	if($right < $left) return Null;
 	if($left == $right){
 		$node = new TreeNode($list[$left]);
 		return $node;
@@ -25,8 +25,38 @@ function sortedArrayToBST($list,$left,$right){
 }
 
 
-function isValidBST()
+function isValidBSTByInOrder($node){
+	$inOrderList = [];
+	inOrderRecursion($node,$inOrderList);
+	print_r($inOrderList);
+	$count = count($inOrderList);
+	$index = 0;
+	while($index < ($count -1)){
+		if( $inOrderList[$index] < $inOrderList[$index+1]){
+			$index++;
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
+}
+
+function inOrderRecursion($node,&$inOrderList){
+	if($node == NULL){
+		return null;
+	}
+	if($node->left != NULL){
+		inOrderRecursion($node->left,$inOrderList);
+	}
+	$inOrderList[] = $node->value;
+	if($node->right !=NULL){
+		inOrderRecursion($node->right,$inOrderList);
+	}
+}
 
 $list = range(1,10);
 var_dump($list);
 $root = sortedArrayToBST($list,0,9);
+$isvalid = isValidBSTByInOrder($root);
+print_r($isvalid);
